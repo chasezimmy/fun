32f30894161a0d93abde1958283b54352105fd8b
chasezimmy <chasezimmy@gmail.com>
2018-06-15 13:52:49 -0400

    change file name, add another solution

--- list_element_sum_to_k.py
+++ list_element_sum_to_k.py
@@ -5,14 +5,17 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 """
 
-def add_to_k(list, k):
+def sum_to_k(list, k):
+    return any([True if k - _ in list else False for _ in list])
+
+
+def sum_to_k_one_pass(list, k):
     for _ in list:
         if k - _ in list:
             return True
     return False
 
-
 list = [10, 15, 3, 7]
 k = 17
 
-print(add_to_k(list, k))
+print(sum_to_k(list, k))
