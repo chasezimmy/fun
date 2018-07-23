bcc2df64a73bf629bc82b65ffea8203e3480b0ae
chasezimmy <chasezimmy@gmail.com>
2018-07-23 11:46:06 -0400

    test

--- remove_duplicate_char_string/remove_duplicate_char_string.php
+++ remove_duplicate_char_string/remove_duplicate_char_string.php
@@ -1,14 +1,25 @@
+<?php
 // Remove Duplicate Character in String
 
 class Solution {
-    
 
-    public function removeDuplicateCharacter($string) {
+    public function removeDuplicate($string) {
+        $char_in_string = [];
+        $seen = [];
+        $chars = str_split($string);
 
-    }  
+        foreach($chars as $char) {
+            if (!isset($seen[$char])) {
+                $char_in_string[] = $char;
+                $seen[$char] = 1;
+            }
+        }
+
+        return $char_in_string;
+    }
 
 }
 
-
 $string = "Tree Traversal";
-sol = new Solution();
+$sol = new Solution();
+print implode('',$sol->removeDuplicate($string));
