48bd4bbdd2b7ede53fbbd7a7be73a577992a90e3
chasezimmy <chasezimmy@gmail.com>
2018-08-30 23:37:37 -0400

    diff array test

--- longest_increasing_subsequence/longest_increasing_subsequence.py
+++ longest_increasing_subsequence/longest_increasing_subsequence.py
@@ -17,6 +17,6 @@ class Solution:
 		return cache
 
 sol = Solution()
-arr = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
-
+#arr = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
+arr = [3, 4, -1, 0, 6, 2, 3]
 print(sol.longest_increasing_subsequence(arr))
