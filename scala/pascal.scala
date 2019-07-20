236a7fd8279c2ef952682ae27428de4c4b2b5e33
chasezimmy <chase.zimmerman@modaoperandi.com>
2019-07-19 23:54:52 -0400

    abomination cleanup

--- scala/pascal.scala
+++ scala/pascal.scala
@@ -1,34 +1,40 @@
 import scala.collection.mutable.ArrayBuffer
 
-def pascal(C: Int, R: Int): Unit = {
+// Recursion
+def pascal(row: Int, column: Int): Int =
+    if (row == column || column == 0) 1
+		else pascal(column - 1, row - 1) + pascal(column, row - 1) 
+
+pascal(1, 2) // 2
+
+// What have I done?
+def pascalTerrible(column: Int, row: Int): Int = {
   var P: ArrayBuffer[Int] = ArrayBuffer()
   var c: Int = 0
   var r: Int = 0
+  
 	def calc(i: Int): Int = {
-    if (c == 0) 1
-    else if (c == r) 1
+    if (c == 0 or c == r) 1
     else P(i - r - 1) + P(i - r)
   }
     
-  def loop: Unit = {
-    for (i <- 0 to 20) {
+  def loop: Int = {
+    var i: Int = 0
+    while (!(column == c && row == r)) {
       P += calc(i)
-      if (C == c && R == r) println(calc(i))
-      
+      i = i + 1
       if (c > r) {
         r = r + 1
-      }
-      else if (c == r) {
-        r = r +1
-        c = 0 
-      }
-      else if (c < r) {
+      } else if (c < r) {
         c = c + 1
+      } else {
+        r = r +1
+        c = 0  
       }
-    }
+    } 
+    calc(i)
   }
   loop
-  
 }
   
-pascal(1, 2)
+pascalTerrible(1, 2) // 2
